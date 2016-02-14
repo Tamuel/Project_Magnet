80bca7c2d51ec9d35b81849088d167da415bc86f
Tamuel <eastern7star@gmail.com>
2016-02-14 19:33:50 +0900

    척력 계산식 수정, 극성 바꿀 수 있도록 수정

--- Build/Android/src/com/YourCompany/Magnet/OBBData.java
+++ Build/Android/src/com/YourCompany/Magnet/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-315816449L // the length of the file in bytes
+188559825L // the length of the file in bytes
 )
 };
 };
